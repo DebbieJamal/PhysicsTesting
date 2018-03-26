7a29276b17c64c83d90e87389e980c72c68d226a
DebbieJamal <debbiexjamal@gmail.com>
2018-03-26 13:25:17 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const sqlite = require('sqlite3');
+const db = new sqlite.Database('./testdb.sqlite');
+// const { printQueryResults } = require('./utils'); //
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4001;
+// Use static server to serve the Express Yourself Website
+app.use(express.static('public'));
+
+// Open a call to `app.get()` below:
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
